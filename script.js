f8c50d3ec398afea9711295e6f620815d9ccd01e
Pavel <47458116+PavlikFilippich@users.noreply.github.com>
2019-03-19 21:01:39 +0300

    Даны нормальные названия функциям

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-class Cultum{
+class СlassificationAnimal{
     constructor(){
     this.one = document.querySelector('.one');
     this.two = document.querySelector('.two');
     this.three = document.querySelector('.three');
     this.animal = document.querySelector('.animal');
     this.message = document.querySelector('.message');
-    this.one.addEventListener('click', ()=>{ this.getValue();this.se()});
-    this.two.addEventListener('click', ()=>{ this.getValue();this.ve()});
-    this.three.addEventListener('click', ()=>{ this.getValue();this.ge()});
+    this.one.addEventListener('click', ()=>{ this.getValue();this.information_one()});
+    this.two.addEventListener('click', ()=>{ this.getValue();this.information_two()});
+    this.three.addEventListener('click', ()=>{ this.getValue();this.information_three()});
     this.message.addEventListener('click',this.removemessage.bind(this));
     }
     getValue() {
@@ -20,41 +20,41 @@ class Cultum{
         setTimeout(()=>this.message.classList.remove(pos),3000);
     }
     
-    se(){
+    information_one(){
         let text, pos = 'message__one';
-        if (this.select == 'animal__fox') {         
+        if (this.select === 'animal__fox') {         
             text = 'Лиса хитрый зверь';
         }
-        else if (this.select == 'animal__wolf') {
+        else if (this.select === 'animal__wolf') {
             text = 'Волк вожак стаи';
         }
-        else if (this.select == 'animal__peppa') {
+        else if (this.select === 'animal__peppa') {
             text = 'Кабан дикий свин';
         }
         this.alert_text(pos,text); 
     }
-    ve(){
+    information_two(){
         let text, pos = 'message__two';
-        if (this.select == 'animal__fox') {
+        if (this.select === 'animal__fox') {
             text = 'Он ничем не отличался от ста тысяч других лисиц';
         }
-        else if (this.select == 'animal__wolf') {
+        else if (this.select === 'animal__wolf') {
             text = 'Волк собирает грибы в лесу';
         }
-        else if (this.select == 'animal__peppa') {
+        else if (this.select === 'animal__peppa') {
             text ='Кабан свинья, которая не поддалась на уговоры';
         }
         this.alert_text(pos,text);
     }
-    ge(){
+    information_three(){
         let text, pos = 'message__three';
-        if (this.select == 'animal__fox') {
+        if (this.select === 'animal__fox') {
             text = 'Рыжий лис';
         }
-        else if (this.select == 'animal__wolf') {
+        else if (this.select === 'animal__wolf') {
             text = 'Волк меняет шкуру, но не нрав';
         }
-        else if (this.select == 'animal__peppa') {
+        else if (this.select === 'animal__peppa') {
             text = 'Кабан санитар леса';
         }
         this.alert_text(pos,text);
@@ -65,4 +65,4 @@ class Cultum{
         this.message.classList.remove('message__three');
     }
 }
-window.onload = new Cultum();
+window.onload = new СlassificationAnimal();
